0e8a25aae8ce9afca9dd4bd8cd3fd9958a711988
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-04-30 23:58:25 +0530

    BookingSet

--- configs/models/Booking.js
+++ configs/models/Booking.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-    car:{type: isObjectId, ref: "Car", required: true},
-    user:{type: isObjectId, ref: "User", required: true},
-    Owner:{type: isObjectId, ref: "Car", required: true},
-    pickupDate : {type: Date, required: true},
-    returnDate : {type: Date, required: true},
+    car: {type: mongoose.Schema.Types.ObjectId, ref: "Car", required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    pickupDate: {type: Date, required: true},
+    returnDate: {type: Date, required: true},
     status: {type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending'},
-    price : {type: Number, required: true},
-    
-   
-   
+    price: {type: Number, required: true},
+},{
     timestamps: true
 })
 
 const Booking = mongoose.model("Booking", bookingSchema)
 
-export default Car
+export default Booking
